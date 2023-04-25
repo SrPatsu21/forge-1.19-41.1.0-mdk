c131df5f659d3f5d1469bbfd8b1e8f592355017a
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-04-25 14:57:07 -0300

    falta ainda

--- src/main/java/com/github/uranus_mod_group/uranus_mod/event/ClientEvents.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/event/ClientEvents.java
@@ -1,4 +1,28 @@
 package com.github.uranus_mod_group.uranus_mod.event;
 
+import com.github.uranus_mod_group.uranus_mod.Uranus_mod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.swing.text.JTextComponent;
+
 public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = Uranus_mod.ModId, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = Uranus_mod.ModId, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            //event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
 }
