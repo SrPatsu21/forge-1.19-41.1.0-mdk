4585bdf7c22673276237f0281d611aecd95af7ed
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-04-25 22:38:00 -0300

    villargers still don`t work

--- src/main/java/com/github/uranus_mod_group/uranus_mod/event/ModEvents.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/event/ModEvents.java
@@ -1,24 +1,59 @@
 package com.github.uranus_mod_group.uranus_mod.event;
 
 import com.github.uranus_mod_group.uranus_mod.Uranus_mod;
+import com.github.uranus_mod_group.uranus_mod.item.ModItems;
 import com.github.uranus_mod_group.uranus_mod.mana.PlayerMana;
 import com.github.uranus_mod_group.uranus_mod.mana.PlayerManaProvider;
+import com.github.uranus_mod_group.uranus_mod.villarger.ModVillargers;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.item.trading.MerchantOffer;
+
 
 import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Uranus_mod.ModId)
 public class ModEvents {
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event){
+        if(event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.ALCHEMICAL_TOME.get(), 1);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    stack,10,8,0.02F));
+        }
+
+        if(event.getType() == ModVillargers.FIGHT_MASTER.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.ALCHEMICAL_TOME.get(), 15);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    stack,10,8,0.02F));
+        }
+    }
+
+
     @SubscribeEvent
     public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
         if(event.getObject() instanceof Player) {
--- src/main/java/com/github/uranus_mod_group/uranus_mod/villarger/ModVillargers.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/villarger/ModVillargers.java
@@ -3,6 +3,9 @@ package com.github.uranus_mod_group.uranus_mod.villarger;
 import com.github.uranus_mod_group.uranus_mod.Uranus_mod;
 import com.github.uranus_mod_group.uranus_mod.block.ModBlocks;
 import com.google.common.collect.ImmutableSet;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +25,10 @@ public class ModVillargers {
             () -> new PoiType(ImmutableSet.copyOf(ModBlocks.SWORD_SUPPORT.get().getStateDefinition().getPossibleStates()),1, 1)
     );
 
+    public static final RegistryObject<VillagerProfession> FIGHT_MASTER = PROFESSIONS.register("fight_master",
+            () -> new VillagerProfession( "Fight Master", x -> x.get() == SWORD_SUPPORT_POI.get(),
+                    x -> x.get() == SWORD_SUPPORT_POI.get(), ImmutableSet.of(), ImmutableSet.of(),
+                    SoundEvents.VILLAGER_WORK_TOOLSMITH));
 
     public static void registerPOis() {
         try {
