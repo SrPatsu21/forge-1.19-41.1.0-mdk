2b09cb39f054c321f35eae3131c6e418007fb3a9
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-04-27 20:29:29 -0300

    mana beta update, key remaster

--- src/main/java/com/github/uranus_mod_group/uranus_mod/util/KeyBinding.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/util/KeyBinding.java
@@ -15,7 +15,7 @@ public class KeyBinding {
     public static final KeyMapping SKILLS_1_KEY = new KeyMapping(KEY_SKILLS_1, KeyConflictContext.IN_GAME,
             InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, KEY_CATEGORY_URANUS);
     public static final KeyMapping SKILLS_2_KEY = new KeyMapping(KEY_SKILLS_2, KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, KEY_CATEGORY_URANUS);
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_X, KEY_CATEGORY_URANUS);
     public static final KeyMapping SKILLS_3_KEY = new KeyMapping(KEY_SKILLS_3, KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, KEY_CATEGORY_URANUS);
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_C, KEY_CATEGORY_URANUS);
 }
