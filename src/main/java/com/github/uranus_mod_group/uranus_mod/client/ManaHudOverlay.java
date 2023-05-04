7b226bf460b538093250cb89a37e12e2d73acd65
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-05-03 22:20:29 -0300

    correcao de texto

--- src/main/java/com/github/uranus_mod_group/uranus_mod/client/ManaHudOverlay.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/client/ManaHudOverlay.java
@@ -1,33 +1,34 @@
 package com.github.uranus_mod_group.uranus_mod.client;
 
 import com.github.uranus_mod_group.uranus_mod.Uranus_mod;
+import com.github.uranus_mod_group.uranus_mod.mana.PlayerMana;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 public class ManaHudOverlay {
-    private static final ResourceLocation FILLED_MANA = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/filled_mana.png");
-    private static final ResourceLocation EMPTY_MANA = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/empty_mana.png");
+    private static final ResourceLocation MANA_BAR_CONTENT = new ResourceLocation(Uranus_mod.ModId,
+            "textures/mana/mana_bar_content-56x8.png");
+    private static final ResourceLocation MANA_BAR_STRUCTURE = new ResourceLocation(Uranus_mod.ModId,
+            "textures/mana/mana_bar_structure-56x8.png");
 
     public static final IGuiOverlay HUD_MANA = ((gui, poseStack, partialTick, width, height) -> {
-        //empty mana
-        RenderSystem.setShaderTexture(0, EMPTY_MANA);
-        for(int i = 0; i < 10; i++) {
-            GuiComponent.blit(poseStack,width - 620 + (i * 9), height - 16,0,0,12,12,
-                    12,12);
-        }
         //filled mana
-        RenderSystem.setShaderTexture(0, FILLED_MANA);
-        for(int i = 0; i < 10; i++) {
+        RenderSystem.setShaderTexture(0, MANA_BAR_CONTENT);
+
+        for(int i = 0; i < 56; i++) {
+
             if(ClientManaData.getPlayerMana() > i) {
-            GuiComponent.blit(poseStack,width - 620 + (i * 9), height - 16,0,0,12,12,
-                    12,12);
+                GuiComponent.blit(poseStack,width - 620, height - 16,0,0,1+i,8,
+                        1+i,8);
             } else {
                 break;
             }
         }
+        //empty mana
+        RenderSystem.setShaderTexture(0, MANA_BAR_STRUCTURE);
+            GuiComponent.blit(poseStack,width - 620, height - 16,0,0,56,8,
+                    56,8);
     });
 }
