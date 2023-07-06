cbc0a70263071ed732f5208de9bfc9d4f2efc06b
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-07-06 11:05:43 -0300

    ajustes em PlayerMana

--- src/main/java/com/github/uranus_mod_group/uranus_mod/mana/PlayerMana.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/mana/PlayerMana.java
@@ -3,16 +3,14 @@ package com.github.uranus_mod_group.uranus_mod.mana;
 import net.minecraft.nbt.CompoundTag;
 
 public class PlayerMana {
-    //base stats
-    private int mana;
+    //mana level
     private int ml = 0;
     private final int MAX_ML = 2147483647;
-
     //mana
+    private int mana;
     private int Mbase = 100;
     private final int MIN_MANA = 0;
     private int MAX_MANA = (int) (Mbase + (Mbase*(0.5f * ml)));
-
     //functions
     public int getMana(){return mana;}
     public int getMl(){return ml;}
