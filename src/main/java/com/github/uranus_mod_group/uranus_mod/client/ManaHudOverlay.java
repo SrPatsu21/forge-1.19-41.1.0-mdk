46bad05237d26dad8a33603b9573849cf2c3abcb
matheuswogt37 <matheusnotas07@gmail.com>
2023-07-09 17:44:46 -0300

    ajeitado a mana_bar que não agia de acordo com a mana

--- src/main/java/com/github/uranus_mod_group/uranus_mod/client/ManaHudOverlay.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/client/ManaHudOverlay.java
@@ -10,38 +10,49 @@ import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 public class ManaHudOverlay {
     private static final ResourceLocation MANA_BAR_MANA = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/mana_bar_mana-120x26.png");
+            "textures/mana/mana_bar_mana-99x23.png");
     private static final ResourceLocation MANA_BAR_STRUCTURE = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/mana_bar_structure-120x26.png");
+            "textures/mana/mana_bar_structure-101x23.png");
 
     private static final ResourceLocation MANA_BAR_BACKGROUND = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/mana_bar_background-120x26.png");
-
-    private static final ResourceLocation MANA_BAR_MARCADORES = new ResourceLocation(Uranus_mod.ModId,
-            "textures/mana/mana_bar_marcadores-120x26.png");
+            "textures/mana/mana_bar_background-101x23.png");
 
     public static final IGuiOverlay HUD_MANA = ((gui, poseStack, partialTick, width, height) -> {
         int x = width/3;
         int y = height;
-        int tam_bar_x = 120;
-        int tam_bar_y = 26;
+        int tam_bar_x = 101;
+        int tam_bar_y = 23;
 
+        /*
+        GuiComponent.blit(
+            poseStack -> indica que vai ser em matriz,
+            posicao inicial em x,
+            posicao inicial em y,
+            naosei,
+            naosei,
+            tamanho em x,
+            tamanho em y,
+            naosei,
+            naosei
+        )
+         */
+
+        //background
         RenderSystem.setShaderTexture(0, MANA_BAR_BACKGROUND);
         GuiComponent.blit(poseStack,x - tam_bar_x, y - tam_bar_y,0,0, tam_bar_x,tam_bar_y,
                 tam_bar_x,tam_bar_y);
-        //filled mana
 
+        //mana
         PlayerMana mana = new PlayerMana();
         RenderSystem.setShaderTexture(0, MANA_BAR_MANA);
-        int size = (int) (ClientManaData.getPlayerMana()*tam_bar_x)/mana.getMaxMana();
-        GuiComponent.blit(poseStack,x - tam_bar_x, y - tam_bar_y,0,0, size,tam_bar_y,
+        int size = (int) (ClientManaData.getPlayerMana()*(tam_bar_x - 2))/mana.getMaxMana();
+        GuiComponent.blit(poseStack,(x - tam_bar_x) + 1, y - tam_bar_y,0,0, size,tam_bar_y,
                 tam_bar_x,tam_bar_y);
 
-        RenderSystem.setShaderTexture(0, MANA_BAR_MARCADORES);
-        GuiComponent.blit(poseStack,x - tam_bar_x, y - tam_bar_y,0,0, tam_bar_x,tam_bar_y,
-                tam_bar_x,tam_bar_y);
+        //mana value bellow mana_bar
 
-        //empty mana
+
+        //structure (details)
         RenderSystem.setShaderTexture(0, MANA_BAR_STRUCTURE);
         GuiComponent.blit(poseStack,x - tam_bar_x, y - tam_bar_y,0,0, tam_bar_x,tam_bar_y,
                 tam_bar_x,tam_bar_y);
