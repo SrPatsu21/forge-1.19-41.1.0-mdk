d0530357057d464de866e85106cc3f979a18a393
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-07-12 14:08:29 -0300

    ok agora eu n sei mais o que fazer para corrigir o bug de geracao de mob

--- src/main/java/com/github/uranus_mod_group/uranus_mod/entity/client/MagicSphereRender.java
+++ src/main/java/com/github/uranus_mod_group/uranus_mod/entity/client/MagicSphereRender.java
@@ -6,7 +6,7 @@ import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public abstract class MagicSphereRender extends ArrowRenderer<MagicSphereEntity>
+public class MagicSphereRender extends ArrowRenderer<MagicSphereEntity>
 {
     public static final ResourceLocation MAGIC_SPHERE_LOCATION = new ResourceLocation(Uranus_mod.ModId, "textures/entity/spells/magic_sphere_texture.png");
 
